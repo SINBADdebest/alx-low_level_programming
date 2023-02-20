8b06d879567960197928baa459dc197389a9000c
SINBADdebest <nodoubtiamebbest@gmail.com>
2023-02-19 16:22:22 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that print usining put
- * Return: 0
+ * Return 0
  */
 int main(void)
 {
-puts("with proper grammar, but the outcome is a piece of art,");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
 
