b4435401b6a53943508a2a0add778ddfe27153d3
SINBADdebest <nodoubtiamdebest@gmail.com>
2023-07-13 15:55:30 -0700

    1 to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-using 'puts' to print a text in a line
+ * returnin: 0 =success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
