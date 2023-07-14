a666bc402d01315c67195b3ab513e7e00ec3c579
SINBADdebest <nodoubtiamdebest@gmail.com>
2023-07-13 18:02:01 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int b;
 	float n;
 
-prinf("size of char: %lu byte(s)\n", (unsigned long)sizeof(z));
-prinf("size of int: %lu byte(s)\n", (unsigned long)sizeof(x));
-prinf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(v));
-prinf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-prinf("size of float: %lu byte(s)\n", (unsigned long)sizeof(n));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(z));
+printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(x));
+printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(v));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(n));
 return (0);
 }
