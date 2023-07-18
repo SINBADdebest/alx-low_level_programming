d54cebfecf52938db824660396fea4d1f8985512
SINBADdebest <nodoubtiamdebest@gmail.com>
2023-07-17 19:32:57 -0700

    100 to 102

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,11 +14,11 @@ int main(void)
 		{
 			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit1 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+				putchar(digit3 + '0');
 
-				if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+				if (!(digit1 == 7 && digit2 == 8 && digit3 == 9))
 				{
 					putchar(',');
 					putchar(' ');
@@ -26,6 +26,7 @@ int main(void)
 			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
